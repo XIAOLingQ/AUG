4dc4fbf841914a0f829a192d654c6aab6fb11875
孙培伶 <3061784569@qq.com>
2024-10-03 12:43:56 +0800

    init

---
+++ ce.py
@@ -0,0 +1,11 @@
+import requests
+import json
+
+def get_completion(prompt):
+    headers = {'Content-Type': 'application/json'}
+    data = {"prompt": prompt, "history": []}
+    response = requests.post(url='http://127.0.0.1:6006', headers=headers, data=json.dumps(data))
+    return response.json()['response']
+
+if __name__ == '__main__':
+    print(get_completion('你好，讲个幽默小故事'))
